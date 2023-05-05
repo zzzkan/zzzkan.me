0dcfd12d9148533fbf9a2cf8db9b872bb607a81b
zzzkan <zzz602214076@outlook.com>
2023-05-05 17:40:47 +0900

    chore

--- src/@zzzkan/gatsby-theme-blog/components/Layout/Footer.tsx
+++ src/@zzzkan/gatsby-theme-blog/components/Layout/Footer.tsx
@@ -1,22 +1,37 @@
-import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
-import { Box, Center, Flex, Heading, HStack, Spacer, Text } from "@chakra-ui/react"
-import { Copyright } from "@zzzkan/gatsby-theme-blog/src/components/Copyright"
-import { ExternalLinks } from "@zzzkan/gatsby-theme-blog/src/components/ExternalLinks"
-import { type SiteMetadataType } from "@zzzkan/gatsby-theme-blog/src/types/siteMetadataType"
-import { type ThemeOptionType } from "@zzzkan/gatsby-theme-blog/src/types/themeOptionType"
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { Copyright } from "@zzzkan/gatsby-theme-blog/src/components/Copyright";
+import { ExternalLinks } from "@zzzkan/gatsby-theme-blog/src/components/ExternalLinks";
+import { type SiteMetadataType } from "@zzzkan/gatsby-theme-blog/src/types/siteMetadataType";
+import { type ThemeOptionType } from "@zzzkan/gatsby-theme-blog/src/types/themeOptionType";
 
 type Props = Pick<SiteMetadataType, "author" | "publicationYear"> &
-  Pick<ThemeOptionType, "links">
+  Pick<ThemeOptionType, "links">;
 
 export const Footer: React.FC<Props> = ({ author, publicationYear, links }) => {
   return (
     <Box as={"footer"} position={"sticky"} top={"100vh"}>
       <Center marginBottom={3}>
         <HStack>
-          <StaticImage src="../../../../../contents/images/icon.png" alt="zzzkan" width={80} style={{ borderRadius: "50%" }}/>
+          <StaticImage
+            src="../../../../../contents/images/icon.png"
+            alt="zzzkan"
+            width={60}
+            style={{ borderRadius: "50%" }}
+          />
           <div>
-            <Heading as={"div"} fontSize={"lg"} fontWeight={"semibold"}>zzzkan</Heading>
+            <Heading as={"div"} fontSize={"lg"} fontWeight={"semibold"}>
+              zzzkan
+            </Heading>
             <Text fontSize={"sm"}>アルフォートは水色派です。</Text>
           </div>
         </HStack>
@@ -27,5 +42,5 @@ export const Footer: React.FC<Props> = ({ author, publicationYear, links }) => {
         <ExternalLinks links={links} />
       </Flex>
     </Box>
-  )
-}
+  );
+};
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,27 @@
-import React from "react"
-import { HeadFC, Link as GatsbyLink } from "gatsby"
-import { Box, Heading, Link, Text } from "@chakra-ui/react"
-import { Layout } from "@zzzkan/gatsby-theme-blog/src/components/Layout"
+import React from "react";
+import { HeadFC, Link as GatsbyLink } from "gatsby";
+import { Box, Heading, Link, Text } from "@chakra-ui/react";
+import { Layout } from "@zzzkan/gatsby-theme-blog/src/components/Layout";
 
 const NotFoundPage: React.FC = () => {
   return (
     <Layout>
       <Box textAlign={"center"}>
-        <Heading as={"h1"} size={"xl"}marginBottom={3}>
+        <Heading as={"h1"} size={"xl"} marginBottom={3}>
           404
         </Heading>
-        <Text marginBottom={12}>このページはすでに削除されているかURLが間違っている可能性があります。</Text>
-        <Link as={GatsbyLink} to={"/"} marginX={"auto"} color={"tint"}>トップへ戻る</Link>
-      </Box>  
+        <Text marginBottom={12}>
+          このページはすでに削除されているかURLが間違っている可能性があります。
+        </Text>
+        <Link as={GatsbyLink} to={"/"} marginX={"auto"} color={"tint"}>
+          トップへ戻る
+        </Link>
+      </Box>
     </Layout>
-  )
-}
+  );
+};
 
-
-export default NotFoundPage
+export default NotFoundPage;
 
 export const Head: HeadFC = () => {
   return (
@@ -26,5 +29,5 @@ export const Head: HeadFC = () => {
       <title>ページが見つかりませんでした - blog.zzzkan.me</title>
       <meta name="robots" content="noindex, nofollow" />
     </>
-  )
-}
+  );
+};
