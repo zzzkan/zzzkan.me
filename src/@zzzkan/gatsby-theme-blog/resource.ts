1ede214f0bf95d96efbb018a1232ab7eaa15c3a1
zzzkan <zzz602214076@outlook.com>
2023-07-08 13:08:19 +0900

    update

---
+++ src/@zzzkan/gatsby-theme-blog/resource.ts
@@ -0,0 +1,13 @@
+export const resource = {
+  FeaturedImageAltText: "アイキャッチ画像",
+  FeaturedImageLinkAltText: (pageName: string) =>
+    `アイキャッチ画像 (${pageName})`,
+  ToggleColorModeLabelText: "カラーモードを変更",
+  AllPostsText: "記事一覧",
+  PreviousPageText: "前の記事",
+  NextPageText: "次の記事",
+  RelatedPostsText: "関連記事",
+  PageCountText: (count: number) => `${count}ページ目`,
+  TagPostsDescriptionText: (tagName: string) =>
+    `${tagName}タグが付いた記事一覧`,
+};
