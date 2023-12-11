e0330871fabce3386dcf3a76523d5ff39fe6cfbc
zzzkan <zzz602214076@outlook.com>
2023-12-12 02:30:36 +0900

    update header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Link as GatsbyLink } from "gatsby";
 import { HeadFC } from "gatsby";
-import { Box, Center, Flex, Heading, HStack, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import { Layout } from "@zzzkan/gatsby-theme-blog/src/components/Layout";
 import { Seo } from "@zzzkan/gatsby-theme-blog/src/components/Seo";
 
@@ -10,10 +18,13 @@ const HomePage: React.FC = () => {
     <Layout>
       <Flex alignItems={"center"} justifyContent={"center"} height={"50vh"}>
         <Box>
-          <Heading as={"h1"} size={"4xl"} textAlign={"center"} marginBottom={3}>
-            Home
+          <Heading as={"h1"} size={"4xl"}>
+            zzzkan.me
           </Heading>
-          <Center marginBottom={3}>
+          <Text fontSize={"sm"} marginBottom={3}>
+            zzzkan's website
+          </Text>
+          <Center>
             <HStack spacing={"6"}>
               <Link as={GatsbyLink} to="/about" color={"tint"} fontSize={"xl"}>
                 About
