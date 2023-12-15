3ff13865f392ec371ed9b6452bc404fddbd00927
zzzkan <zzz602214076@outlook.com>
2023-12-16 02:10:21 +0900

    add font

---
+++ gatsby-browser.ts
@@ -0,0 +1,2 @@
+import "@fontsource-variable/m-plus-2";
+import "@fontsource-variable/m-plus-1-code";
---
+++ src/@zzzkan/gatsby-theme-blog/theme/fonts.ts
@@ -0,0 +1,7 @@
+const fonts = {
+  heading: `"M PLUS 2 Variable", sans-serif`,
+  body: `"M PLUS 2 Variable", sans-serif`,
+  mono: `"M PLUS 1 Code Variable",monospace`,
+};
+
+export default fonts;
